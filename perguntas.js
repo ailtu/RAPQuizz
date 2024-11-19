e6659a202aac26de961aa7c2a89398ca641512f0
Ailton <ailtonc.n64@gmail.com>
2024-11-19 16:20:15 -0300

    perguntas e respostas adicionadas

---
+++ perguntas.js
@@ -0,0 +1,154 @@
+
+export default [
+
+    {
+        question: "Quantos tiros o 50 Cent levou no atendado que quase tirou sua vida?",
+        answers: [
+            { option: "2", correct: false },
+            { option: "9", correct: true },
+            { option: "4", correct: false },
+            { option: "1", correct: false },
+        ],
+    },
+
+    {
+        question: "Quem é o principal suspeito de ter mandado assassinar o 2Pac e o Biggie Smalls?",
+        answers: [
+            { option: "Big L", correct: false },
+            { option: "Suge Knight", correct: false },
+            { option: "P. Diddy", correct: true },
+            { option: "50 Cent", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual era o principal integrante do grupo N.W.A na sua formação original?",
+        answers: [
+            { option: "Eazy-e", correct: true },
+            { option: "Mc Ren", correct: false },
+            { option: "Dr. Dre", correct: false },
+            { option: "Ice Cube", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual o ano do lançamento do álbum 'Get Rich or Die Tryin'' do 50 Cent?",
+        answers: [
+            { option: "2000", correct: false },
+            { option: "2004", correct: true },
+            { option: "2002", correct: false },
+            { option: "1999", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual trio é associado à gravadora Death Row?",
+        answers: [
+            { option: "Dr. Dre, Eminem e 50 Cent", correct: true },
+            { option: "Dr. Dre, Ice Cube e Snoop Dogg", correct: false },
+            { option: "Dr. Dre, Tupac e Snoop Dogg", correct: false },
+            { option: "Eminem, 50 Cent e Snoop Dogg", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual ex-integrante do N.W.A lançou a Diss chamada de 'No Vaseline'?",
+        answers: [
+            { option: "Mobb Deep", correct: false },
+            { option: "Dr. Dre", correct: false },
+            { option: "Eazy-e", correct: false },
+            { option: "Ice Cube", correct: true },
+        ],
+    },
+
+    {
+        question: "Qual música desencadeou a maior guerra entre regiões e rappers nos anos 90?",
+        answers: [
+            { option: "No Vaseline - Ice Cube", correct: false },
+            { option: "Who Shot Ya' – Notorious BIG", correct: true },
+            { option: "Hit 'em Up - 2Pac", correct: false },
+            { option: "All Eyez on Me - 2Pac", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual rapper feminina é conhecida como a 'Rainha do Rap' e lançou hits como 'Anaconda'?",
+        answers: [
+            { option: "Cardi B", correct: false },
+            { option: "Nicki Minaj", correct: true },
+            { option: "Ashanti", correct: false },
+            { option: "Cassie", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual rapper é conhecida por ser uma pioneira no hip-hop feminino nos anos 2000?",
+        answers: [
+            { option: "Missy Elliott", correct: true },
+            { option: "Ciara", correct: false },
+            { option: "Lil Kim", correct: false },
+            { option: "Aaliyah", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual rapper ganhou o Grammy de Melhor Álbum de Rap em 2019?",
+        answers: [
+            { option: "Travis Scott", correct: false },
+            { option: "Offset", correct: false },
+            { option: "Cardi B", correct: true },
+            { option: "SZA", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual rapper e ativista lançou músicas que abordam temas de desigualdade racial como 'Apeshit'?",
+        answers: [
+            { option: "Cassie", correct: false },
+            { option: "Cardi B", correct: false },
+            { option: "Beyoncé", correct: false },
+            { option: "Rapsody", correct: true },
+        ],
+    },
+
+    {
+        question: "Qual rapper começou sua carreira com o grupo Fugees e depois lançou o aclamado álbum solo 'The Miseducation of...' ?",
+        answers: [
+            { option: "YG Marley", correct: false },
+            { option: "Wyclef Jean", correct: false },
+            { option: "Lauryn Hill", correct: true },
+            { option: "Pras Michel", correct: false },
+        ],
+    },
+
+    {
+        question: "qual foi o trio formado para o feat na música 'Let It Go' da Keyshia Cole?",
+        answers: [
+            { option: "Lil Kim, Eve, Cassie", correct: false },
+            { option: "Keyshia Cole, Aalyiah, Sammie", correct: false },
+            { option: "Keyshia Cole, Beyoncé, Monica", correct: false },
+            { option: "Keyshia Cole, Missy Elliot, Lil Kim", correct: true },
+        ],
+    },
+
+    {
+        question: "Quem é o rapper de Compton que lançou o aclamado álbum 'To Pimp a Butterfly'?",
+        answers: [
+            { option: "Kendrick Lamar", correct: true },
+            { option: "Drake", correct: false },
+            { option: "J. Cole", correct: false },
+            { option: "Nenhuma Alternativa Acima", correct: false },
+        ],
+    },
+
+    {
+        question: "Quem é o rapper conhecido por seu estilo provocador e pelo álbum 'Yeezus'?",
+        answers: [
+            { option: "Kanye West", correct: true },
+            { option: "Jay-Z", correct: false },
+            { option: "Suge Knight", correct: false },
+            { option: "Ye", correct: true },
+        ],
+    },
+
+];
